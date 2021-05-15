d1e605cb402ef21c6abb70035dc3d6b162ea84a6
TrueMerc <true_merc@mail.ru>
2021-05-15 20:38:19 +0300

    Improve profile page appearance

--- react/chat/src/components/Profile.jsx
+++ react/chat/src/components/Profile.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { store } from "../store";
 import { changeProfile } from "../store/chat/actions";
 import "../main.css";
 import { useDispatch, useSelector } from "react-redux";
 
 
-const InputField = ({ fieldName, fieldValue, onChange }) => {
+const InputField = ({ fieldValue, onChange }) => {
 
     const handleInputChange = (e) => {
         onChange(e.target.value);
@@ -13,7 +12,6 @@ const InputField = ({ fieldName, fieldValue, onChange }) => {
 
     return (
         <div className="profile__input-field">
-            <label for="input">{fieldName}:&nbsp;</label>
             <input
                 id="input"
                 type="text"
@@ -30,33 +28,39 @@ const Profile = () => {
     const dispatch = useDispatch();
 
     const handleInputChange = (fieldName) => (fieldValue) => {
-        console.log(`${fieldName} = ${fieldValue}`);
         const newUser = { ...user, [fieldName]: fieldValue };
-        console.log(newUser);
         const change = { ...changeProfile, ['value']: newUser };
-        console.log(change);
         dispatch(change);
     }
 
     const fields = [
         { name: 'Имя', value: user.name, onChange: handleInputChange('name') },
         { name: 'Фамилия', value: user.surname, onChange: handleInputChange('surname') },
-        { name: 'Отображаемое имя', value: user.nickname, onChange: handleInputChange('nickname') },
+        { name: 'Псевдоним', value: user.nickname, onChange: handleInputChange('nickname') },
         { name: 'Страна', value: user.country, onChange: handleInputChange('country') }
     ];
 
     return (
         <div className="main-area">
             <h3>Профиль пользователя</h3>
-            {fields.map((field, index) => (
-                    <InputField
-                        key={`inputField${index}`} 
-                        fieldName={field.name}
-                        fieldValue={field.value}
-                        onChange={field.onChange}
-                    />
-                ))
-            }
+            <table>
+                <tbody>
+                    {fields.map((field, index) => (
+                        <tr key={`inputField${index}`}>
+                            <td>
+                                {field.name}
+                            </td>
+                            <td>
+                                <InputField
+                                    fieldValue={field.value}
+                                    onChange={field.onChange}
+                                />
+                            </td>
+                        </tr>
+                    ))
+                    }
+                </tbody>
+            </table>
         </div>
     );
 }
