54b77f385493c976ad9d3523163cf7d9e219cae3
TrueMerc <true_merc@mail.ru>
2021-05-15 21:58:43 +0300

    Fix messages from human

--- react/chat/src/components/Chat.jsx
+++ react/chat/src/components/Chat.jsx
@@ -1,18 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router";
-import InputForm from "./InputForm.jsx"
-import Messages from "./Messages.jsx"
+import InputForm from "./InputForm.jsx";
+import Messages from "./Messages.jsx";
 import { useDispatch, useSelector } from "react-redux";
+import { changeChats } from "../store/chat/actions";
+import Authors from "../domain/Authors";
 
-const Chat = ({onSubmit }) => {
+const Chat = () => {
     const chats = useSelector(state => state.chats);
     const dispatch = useDispatch();
     const params = useParams();
     const id = Number.parseInt(params.chatId);
 
-    const handleSubmit = () => {
 
+    // // Bot answer    
+    // useEffect(() => {
+    //     console.log(chats);
+    //     console.log(lastChatId);
+    //     if (lastChatId < 0) {
+    //         return;
+    //     }
+    //     const messages = chats[lastChatId].messages;
+    //     if (messages.length > 0) {
+    //         const lastMessage = messages[messages.length - 1];
+    //         if (lastMessage.author === Authors.HUMAN) {
+    //             const answerText = (lastMessage.text.toLocaleLowerCase().search('привет') !== -1)
+    //                 ? 'Привет!'
+    //                 : 'Не могу разобрать сообщение.';
+    //             chats[lastChatId].messages = [...messages, { text: answerText, author: Authors.BOT }];
+    //             setChats([...chats]);
+    //         }
+    //     }
+    // }, [chats, lastChatId]);
+
+
+    const handleSubmit = useCallback((id, message) => {
+        console.log(message);
+        chats[id].messages = [...chats[id].messages, { text: message, author: Authors.HUMAN }];
+        applyChanges([...chats]);
+    });
+
+    const applyChanges = (newChats) => {
+        dispatch({...changeChats, ['value']: newChats});
     }
+
+
 
     return (
         <div className="main-area">
