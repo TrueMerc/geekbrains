2c2d1b1d71c8a0cd88f14db8e5c61c6ed5b35069
TrueMerc <true_merc@mail.ru>
2021-05-18 20:51:56 +0300

    Add Redux development tools support

--- react/chat/src/store/index.js
+++ react/chat/src/store/index.js
@@ -3,8 +3,11 @@ import { chatsReducer } from "./chats/reducer";
 import { profileReducer } from "./profile/reducer";
 
 const rootReducer = combineReducers({
-    chats: chatsReducer, 
+    chats: chatsReducer,
     profile: profileReducer
 });
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
