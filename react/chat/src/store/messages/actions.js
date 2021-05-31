b4d0b596d65e43bbb1ad30226a003d8ecf57f84d
TrueMerc <true_merc@mail.ru>
2021-05-31 15:56:27 +0300

    Remove commented code

--- react/chat/src/store/messages/actions.js
+++ react/chat/src/store/messages/actions.js
@@ -44,14 +44,6 @@ export const addMessageWithThunk = (newMessage, chatId) => (dispatch) => {
         }).catch((error) => {
             console.error(error);
         });
-        // const text = (newMessage.text.toLocaleLowerCase().search('привет') !== -1)
-        //     ? 'Привет!'
-        //     : 'Не могу разобрать сообщение.';
-
-        // const timeout = setTimeout(() => {
-        //     dispatch(addMessage({ text: text, author: Authors.BOT }, chatId));
-        //     dispatch(startBlinkWithThunk({ text: text, author: Authors.BOT }, chatId));
-        // }, 1000);
     }
 }
 
